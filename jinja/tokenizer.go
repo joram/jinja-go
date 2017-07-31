c9966aa615af522cd128e693d52628454acd303c
Daniel Steuernol <daniel@sendwithus.com>
2017-07-30 19:14:21 -0700

    move channel into tokenizer

--- jinja/tokenizer.go
+++ jinja/tokenizer.go
@@ -3,17 +3,18 @@ package jinja
 import (
 	"regexp"
 	"sort"
-	"strings"
 )
 
 type Tokenizer struct {
 	body              string
-	length            int
 	blockBeginIndices []int
+	ch                byte
 
 	currPosition int
 	currLine     int
 	inBlock      bool
+
+	C chan Token
 }
 
 func NewTokenizer(s string) Tokenizer {
@@ -30,22 +31,33 @@ func NewTokenizer(s string) Tokenizer {
 	}
 	sort.Ints(blockBeginIndices)
 
-	return Tokenizer{
-		s,
-		len(s),
-		blockBeginIndices,
-		0,
-		1,
-		false,
+	tokenizer := Tokenizer{
+		body:              s,
+		blockBeginIndices: blockBeginIndices,
+		currPosition:      0,
+		currLine:          1,
+		inBlock:           false,
+		C:                 make(chan Token),
 	}
+	go tokenizer.run()
+	return tokenizer
 }
 
-func (t *Tokenizer) nextStringToken() Token {
+func (t *Tokenizer) run() {
+	token := Token{}
+	for token.Type != EOF {
+		token = t.next()
+		t.C <- token
+	}
+	close(t.C)
+}
+
+func (t *Tokenizer) readExternalContent() Token {
 	for _, beginIndex := range t.blockBeginIndices {
 		if beginIndex >= t.currPosition {
 			return Token{
-				STRING,
-				t.body[t.currPosition:beginIndex],
+				EXTERNAL_CONTENT,
+				t.body[t.currPosition : beginIndex-1],
 				t.currLine,
 			}
 		}
@@ -53,70 +65,102 @@ func (t *Tokenizer) nextStringToken() Token {
 
 	// string until EOF
 	return Token{
-		STRING,
-		t.body[t.currPosition:t.length],
+		EXTERNAL_CONTENT,
+		t.body[t.currPosition:len(t.body)],
 		t.currLine,
 	}
 }
 
-func (t *Tokenizer) nextVariableToken() Token {
-	re := regexp.MustCompile("[a-zA-Z]+")
-	indices := re.FindAllIndex([]byte(t.body[t.currPosition:]), -1)
-
-	if len(indices) == 0 {
-		panic("found no vars!")
+func (t *Tokenizer) readIdentifier() Token {
+	position := t.currPosition - 1
+	for isLetter(t.ch) {
+		t.readChar()
 	}
-
-	firstVariable := indices[0]
-	if firstVariable[0] != 0 {
-		panic("a token should start now")
-	}
-
-	variableLength := firstVariable[1]
-	return Token{
-		VARIABLE,
-		t.body[t.currPosition : t.currPosition+variableLength],
-		t.currLine,
-	}
+	end := t.currPosition - 1
+	return Token{IDENTIFIER, t.body[position:end], t.currLine}
 }
 
 func (t *Tokenizer) next() Token {
-	if t.currPosition == t.length {
+	if t.currPosition == len(t.body) {
 		return Token{Type: EOF}
 	}
 
 	if !t.inBlock {
-		token := t.nextStringToken()
+		token := t.readExternalContent()
 		t.inBlock = true
 		t.currPosition += len(token.Value)
 		return token
 	}
 
-	for operator, tokenType := range operators {
-		if strings.HasPrefix(t.body[t.currPosition:], operator) {
-			token := Token{
-				tokenType,
-				t.body[t.currPosition : t.currPosition+len(operator)],
-				t.currLine,
-			}
-			if token.closesBlock() {
-				t.inBlock = false
-			}
-			t.currPosition += len(operator)
-			return token
+	t.skipWhitespace()
+
+	token := Token{ILLEGAL, string(t.ch), t.currLine}
+	switch t.ch {
+	case '+':
+		token = Token{ADD, ADD, t.currLine}
+	case '{':
+		if t.peek() == '{' {
+			t.readChar()
+			token = Token{EXPRESSION_BEGIN, EXPRESSION_BEGIN, t.currLine}
+		} else {
+			token = Token{LBRACE, LBRACE, t.currLine}
 		}
+	case '}':
+		if t.peek() == '}' {
+			t.readChar()
+			token = Token{EXPRESSION_END, EXPRESSION_END, t.currLine}
+			t.inBlock = false
+		} else {
+			token = Token{RBRACE, RBRACE, t.currLine}
+		}
+	default:
+		token = t.readIdentifier()
 	}
 
-	token := t.nextVariableToken()
-	t.currPosition += len(token.Value)
+	t.readChar()
 	return token
+	//for operator, tokenType := range operators {
+	//	if strings.HasPrefix(t.body[t.currPosition:], operator) {
+	//		token := Token{
+	//			tokenType,
+	//			t.body[t.currPosition : t.currPosition+len(operator)],
+	//			t.currLine,
+	//		}
+	//		if token.closesBlock() {
+	//			t.inBlock = false
+	//		}
+	//		t.currPosition += len(operator)
+	//		return token
+	//	}
+	//}
+	//
+	//token := t.readIdentifier()
+	//t.currPosition += len(token.Value)
+	//return token
 }
 
-func (t *Tokenizer) GetTokens(tokens chan Token) {
-	token := Token{}
-	for token.Type != EOF {
-		token = t.next()
-		tokens <- token
+func (t *Tokenizer) peek() byte {
+	return t.body[t.currPosition]
+}
+
+func (t *Tokenizer) skipWhitespace() {
+	for t.ch == ' ' || t.ch == '\t' || t.ch == '\n' || t.ch == '\r' || t.ch == 0 {
+		if t.ch == '\n' {
+			t.currLine += 1
+		}
+		t.readChar()
 	}
-	tokens <- token
+}
+
+func (t *Tokenizer) readChar() {
+	if t.currPosition >= len(t.body) {
+		t.ch = 0
+	} else {
+		t.ch = t.body[t.currPosition]
+	}
+	t.currPosition += 1
+}
+
+func isLetter(ch byte) bool {
+	return 'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' || ch == '_'
 }
